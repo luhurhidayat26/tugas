a93fb20e0c34ee398006398444e2dffad62d8bcb
luhurhidayat26 <48472366+luhurhidayat26@users.noreply.github.com>
2019-03-12 16:00:37 +0700

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPEhtml>
+
+<html>
+
+<head>
+
+<title>latihan php</title>
+
+</head>
+
+<body>
+
+<?php 
+    echo "<h1><b>Hello World</b><br></h1>";
+	echo "NAMA: Luhur Hidayat<br>";
+	echo "NIM: 16.01.53.0074<br>";
+	echo "progdi: Teknik Informatika";
+	
+?>
+</body>
+</html>
